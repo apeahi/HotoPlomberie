405ce3e5820463af3146815d19fb55f6e2a6ec14
apaeahi <apaeahi.tcc@gmail.com>
2018-06-21 16:53:20 -1000

    correction navbar

--- client/src/components/layouts/NavbarHp.js
+++ client/src/components/layouts/NavbarHp.js
@@ -6,28 +6,31 @@ import React, { Component } from 'react';
 class NavbarHp extends Component {
     render() {
         return ( 
-            <nav id="navigation" class="navbar  bg-secondary navbar-dark sticky-top">
-              
-                <ul class=" mr-auto nav nav-pills">
-                  <li class="nav-item px-3">
-                      <a class="nav-link text-white" href="#description">La Société</a>
-                  </li>
-                  <li class="nav-item px-3">
-                      <a class="nav-link text-white" href="#service">Services</a>
-                  </li>
-                  <li class="nav-item px-3">
-                      <a class="nav-link text-white" href="#gallery">Galerie</a>
-                  </li>
-                  <li class="nav-item px-3">
-                      <a class="nav-link text-white" href="#testimony">Temoignage</a>
-                  </li>
-
-
-                </ul>
-                <a class="btn btn-primary mx-3" href="tel:+68987776383">Appeler</a>
-                <a class="btn btn-primary" href="#devis">Demander un Devis&nbsp;</a>
-          
-            </nav>
+            <nav class="navbar navbar-expand-md bg-secondary navbar-dark sticky-top" id="myScrollspy">
+                <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto nav nav-pills nav-stacked">
+                        <li class="nav-item px-3 active">
+                            <a class="nav-link text-white" href="#description">Description</a>
+                        </li>
+                        <li class="nav-item px-3" >
+                            <a class="nav-link text-white" href="#service">Tache</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link text-white" href="#gallery">Galerie</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link text-white" href="#testimony ">Temoignage</a>
+                        </li>
+                    </ul>
+                    <a class="btn btn-primary mx-3" href="tel:+68987776383">Appeler</a>
+                    <a class="btn btn-primary" href="#devis">Demander un Devis&nbsp;</a>
+                </div>
+                </div>
+          </nav>
   
           )
       }
