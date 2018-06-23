2c79a097691edd40e9ef2b1452f67798c2b11a63
apaeahi <apaeahi.tcc@gmail.com>
2018-06-23 10:52:09 -1000

    deployd

--- client/src/components/layouts/Quotation.js
+++ client/src/components/layouts/Quotation.js
@@ -35,30 +35,30 @@ class Quotation extends Component {
                 <div className="col-md-6">
                   <div className="card">
                     <div className="card-body p-5 text-left">
-                      <form>
+                      <form method="POST" action="https://formspree.io/hotoplomberie.services@gmail.com">
                         <div className="form-group">
                           <label>Nom</label>
-                          <input className="form-control" placeholder="Votre Nom" required="required"/> </div>
+                          <input className="form-control" type="text" name="Nom" placeholder="Votre Nom" required="required"/> </div>
                         <div className="form-group">
                           <label for="exampleInputEmail1" >Email</label>
-                          <input type="email" className="form-control" id="inlineFormInput" placeholder="Votre Email" required="required"/> </div>
+                          <input className="form-control" type="email" name="Email" id="inlineFormInput" placeholder="Votre Email" required="required"/> </div>
                         <div className="form-group">
                           <label for="exampleInputEmail1">Téléphone</label>
-                          <input type="number" className="form-control" id="inlineFormInput" placeholder="Votre Numéro" required="required"/> </div>
+                          <input className="form-control" type="text" name="Téléphone" id="inlineFormInput" placeholder="Votre Numéro" required="required"/> </div>
                         <div className="form-group">
                           <label for="exampleInputEmail1">Objet
                             <br/> </label>
-                          <select className="custom-control custom-select" required="required">
+                          <select className="custom-control custom-select" type="text" name="Objet"  required="required">
                             <option selected="">Ouvrir le menu déroulant</option>
-                            <option value="1">Réparation</option>
-                            <option value="2">Rénovation</option>
-                            <option value="3">Construction</option>
-                            <option value="4">Autres</option>
+                            <option value="Réparation">Réparation</option>
+                            <option value="Rénovation">Rénovation</option>
+                            <option value="Construction">Construction</option>
+                            <option value="Autres">Autres</option>
                           </select>
                         </div>
                         <div className="form-group">
                           <label for="exampleInputEmail1">Texte</label>
-                          <input type="text" className="form-control form-control-sm h-25 text-left" id="inlineFormInput" placeholder="Votre Texte" required="required"/> </div>
+                          <textarea name="message" type="text" className="form-control form-control-sm h-25 text-left" id="inlineFormInput" placeholder="Votre Texte" required="required"/> </div>
                         <button type="submit" className="btn btn-secondary btn-block"  href="mailto:info@pingendo.com">Envoyer</button>
                       </form>
                     </div>
